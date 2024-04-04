c1bc3957da3695c9a1977012323af972352bde59
arkiiteck <arkiiteck@gmail.com>
2024-04-04 01:05:45 -0500

    Fourth exercise solved using reverse and template literals

---
+++ 4-arrays.js
@@ -0,0 +1,3 @@
+myArray=["challenge",4,{name:"array challenge"},"invert",1,["Array","with","Data"]]
+console.log(`Arreglo original: ${myArray}`)
+console.log(`Arreglo invertido: ${myArray.reverse()}`)
